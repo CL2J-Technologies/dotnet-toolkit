70ecf91999db11d1c9cceb88b8f8c4997238c1f6
jfblier <jfblier@gmail.com>
2024-03-17 10:11:28 -0400

    Added ShortGuid to DatabaseUtils

--- cl2j.DataStore/cl2j.DataStore.Database/DatabaseUtils.cs
+++ cl2j.DataStore/cl2j.DataStore.Database/DatabaseUtils.cs
@@ -2,9 +2,16 @@
 {
     public static class DatabaseUtils
     {
+        private static readonly Random random = new Random();
+
         public static string CreateGuid()
         {
             return Guid.NewGuid().ToString();
+        }
+
+        public static string CreateShortGuid()
+        {
+            return random.Next(int.MaxValue).ToString("x"); ;
         }
     }
 }
