adc4a7c19b997806d40682a5da8c1af92add0abd
jfblier <jfblier@gmail.com>
2024-08-30 16:31:58 -0400

    Added CacheableObject

---
+++ cl2j.Tooling/CacheableObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace cl2j.Tooling
+{
+    public abstract class CacheableObject<T>
+    {
+        private readonly CacheLoader cacheLoader;
+        private IEnumerable<T> cache = new List<T>();
+
+        public CacheableObject(string name, TimeSpan refreshInterval, ILogger logger)
+        {
+            cacheLoader = new CacheLoader(name, refreshInterval, async () =>
+            {
+                try
+                {
+                    var sw = Stopwatch.StartNew();
+                    cache = await LoadCache();
+                    logger.LogDebug($"{name} --> {cache.Count()} in {sw.ElapsedMilliseconds}ms");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogCritical(ex, $"{name} --> Unable to load cache");
+                }
+            }, logger);
+        }
+
+        protected abstract Task<IEnumerable<T>> LoadCache();
+
+        protected async Task<IEnumerable<T>> GetCache()
+        {
+            await cacheLoader.WaitAsync();
+            return cache;
+        }
+    }
+}
---
+++ cl2j.Tooling/SerializationUtils.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace cl2j.Tooling
+{
+    public class SerializationUtils
+    {
+        public static TOut DeserializeData<TOut>(string? data) where TOut : new()
+        {
+            if (data == null)
+                return new TOut();
+            return JsonConvert.DeserializeObject<TOut?>(data) ?? new TOut();
+        }
+    }
+}
