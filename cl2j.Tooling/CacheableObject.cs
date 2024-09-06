5b9ccb18cb62f90dba15c73e4986a7f4192e9601
jfblier <jfblier@gmail.com>
2024-09-06 16:12:25 -0400

    CacheableObject : Added RefreshCache method

--- cl2j.Tooling/CacheableObject.cs
+++ cl2j.Tooling/CacheableObject.cs
@@ -5,27 +5,34 @@ namespace cl2j.Tooling
 {
     public abstract class CacheableObject<T>
     {
+        private readonly string name;
+        private readonly ILogger logger;
+
         private readonly CacheLoader cacheLoader;
         private List<T> cache = new();
 
         public CacheableObject(string name, TimeSpan refreshInterval, ILogger logger)
         {
-            cacheLoader = new CacheLoader(name, refreshInterval, async () =>
-            {
-                try
-                {
-                    var sw = Stopwatch.StartNew();
-                    cache = await LoadCache();
-                    logger.LogDebug($"{name} --> {cache.Count} in {sw.ElapsedMilliseconds}ms");
-                }
-                catch (Exception ex)
-                {
-                    logger.LogCritical(ex, $"{name} --> Unable to load cache");
-                }
-            }, logger);
+            cacheLoader = new CacheLoader(name, refreshInterval, async () => { await RefreshCache(); }, logger);
+            this.name = name;
+            this.logger = logger;
         }
 
         protected abstract Task<List<T>> LoadCache();
+
+        protected async Task RefreshCache()
+        {
+            try
+            {
+                var sw = Stopwatch.StartNew();
+                cache = await LoadCache();
+                logger.LogDebug($"{name} --> {cache.Count} in {sw.ElapsedMilliseconds}ms");
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex, $"{name} --> Unable to load cache");
+            }
+        }
 
         protected async Task<List<T>> GetCache()
         {
