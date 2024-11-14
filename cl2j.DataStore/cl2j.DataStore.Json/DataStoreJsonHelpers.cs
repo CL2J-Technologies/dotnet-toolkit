b5dca1b31b809473974f26d5a28c4371af632f53
jfblier <jfblier@gmail.com>
2024-11-14 13:05:31 -0500

    Added DataStoreJsonHelpers

---
+++ cl2j.DataStore/cl2j.DataStore.Json/DataStoreJsonHelpers.cs
@@ -0,0 +1,26 @@
+﻿using cl2j.FileStorage.Core;
+using cl2j.FileStorage.Extensions;
+
+namespace cl2j.DataStore.Json
+{
+    public static class DataStoreJsonHelpers
+    {
+        public static async Task<T> GetOrFetch<T>(this IFileStorageProvider storageProvider, string fileName, Func<Task<T>> fetchDelegate) where T : new()
+        {
+            if (await storageProvider.ExistsAsync(fileName))
+                return await storageProvider.ReadJsonObjectAsync<T>(fileName);
+
+            var o = await fetchDelegate();
+
+            await storageProvider.WriteJsonObjectAsync(fileName, o);
+            return o;
+        }
+
+        public static async Task<T> TryRead<T>(this IFileStorageProvider storageProvider, string fileName, T defaultValue) where T : new()
+        {
+            if (await storageProvider.ExistsAsync(fileName))
+                return await storageProvider.ReadJsonObjectAsync<T>(fileName);
+            return defaultValue;
+        }
+    }
+}
