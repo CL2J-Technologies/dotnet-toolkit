c3b6f97374c9c8ddb58d0a4cf4271bb2dd184438
jfblier <jfblier@gmail.com>
2025-12-21 23:19:59 -0500

    Deprecated cl2j.DatraStore.Database

---
+++ cl2j.DataStore/cl2j.DataStore.Database-deprecated/DatabaseUtils.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace cl2j.DataStore.Database
+{
+    public static class DatabaseUtils
+    {
+        private static readonly Random random = new();
+
+        public static string CreateGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public static string CreateShortGuid()
+        {
+            return random.Next(int.MaxValue).ToString("x"); ;
+        }
+
+        public static string GenerateStringList(IEnumerable<string> values)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var value in values)
+            {
+                if (sb.Length > 0)
+                    sb.Append(", ");
+                sb.Append($"'{value}'");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ cl2j.DataStore/cl2j.DataStore.Database-deprecated/EntityExplicitKey.cs
@@ -0,0 +1,10 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace cl2j.DataStore.Database
+{
+    public class EntityExplicitKey<TKey> : IEntity<TKey>
+    {
+        [ExplicitKey]
+        public TKey Id { get; set; } = default!;
+    }
+}
