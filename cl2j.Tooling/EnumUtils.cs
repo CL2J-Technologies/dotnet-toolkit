cc23822105c37e8cb8fde8a185a4ef478400e4fd
jfblier <jfblier@gmail.com>
2026-01-07 16:20:26 -0500

    Added EnumUtils.ToList

---
+++ cl2j.Tooling/EnumUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace cl2j.Tooling
+{
+    public static class EnumUtils
+    {
+        public static string[] ToList<T>() where T : struct
+        {
+            return Enum.GetNames(typeof(T));
+        }
+    }
+}
