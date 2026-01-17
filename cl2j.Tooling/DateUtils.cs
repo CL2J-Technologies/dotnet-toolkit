03ea84f04c72c73f01f67320f4b11e8a2b7cfde9
jfblier <jfblier@gmail.com>
2026-01-17 16:55:54 -0500

    Added DateUtils

---
+++ cl2j.Tooling/DateUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace cl2j.Tooling
+{
+    public static class DateUtils
+    {
+        public static bool InRange(this DateTimeOffset time, DateTimeOffset? start, DateTimeOffset? end)
+        {
+            if (start.HasValue && start.Value > time)
+                return false;
+            if (end.HasValue && end.Value <= time)
+                return false;
+            return true;
+        }
+    }
+}
